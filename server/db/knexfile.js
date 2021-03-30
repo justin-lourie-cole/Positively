c3e74900ef3cb809f0dffc48b5afdfb0dbfdd5ae
malyz <mattuy001@gmail.com>
2021-03-30 21:04:33 +1300

    Fix production config

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -27,7 +27,15 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'migrations')
+    }
   }
 }
