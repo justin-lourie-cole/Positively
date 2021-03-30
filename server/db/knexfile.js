885ddb7e4a7f709b71cf3c186a95ac7fc9332af4
malyz <mattuy001@gmail.com>
2021-03-30 21:41:17 +1300

    change directory of migration and seed

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -30,8 +30,8 @@ module.exports = {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`,
     migrations: {
-        directory: './data/migrations',
+        directory: './db/migrations',
     },
-    seeds: { directory: './data/seeds' }
+    seeds: { directory: './db/seeds' }
   }
 }
